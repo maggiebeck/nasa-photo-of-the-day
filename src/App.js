b6d248581a8ba078a1f841d05f03c6e2b5f9a535
Margaret <mpbeck@umich.edu>
2019-07-17 16:50:13 -0400

    coponent files added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,24 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <div>
+        <h1>Nasa Photo of The Day</h1>
+      </div>
+      <div className="photooftheday"> </div>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+    
+    
+    <div className="links">
+    Links
+  </div>
+    <div className="footer">
+      Footer
     </div>
+    </div>
+    
   );
 }
 
