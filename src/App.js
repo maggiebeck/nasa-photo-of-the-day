fe7298e4ba397ab0b080c9d9fc86c163060d3b8d
Margaret <mpbeck@umich.edu>
2019-07-18 18:50:57 -0400

    day 1 mvp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, {useEffect, useState} from "react";
 import "./App.css";
 import axios from "axios";
+import Photo from "./components/Photo.js";
 
 
 function App() {
-  const [setNasa, nasa] = useState([]);
+  const [photo, setNasa] = useState([]);
 
-  useEffect (() => { axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-03-14')
+  useEffect (() => { axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-07-18')
 
-  .then(res => {setNasa(res.data)
+  .then(res => {setNasa(res.data);
+                photo(res.image);
       
   })
 
@@ -18,26 +20,23 @@ function App() {
 
   return (
     <div className="App">
-      <div>
+      
         <h1>Nasa Photo of The Day</h1>
-        {console.log(nasa)}
-      </div>
       <div className="photooftheday"> </div>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀
       </p>
       <div className="app"{...App}/>
+      {console.log(photo)}
+      <Photo info={photo}/>
       
     
+      
     
-    <div className="links">
-    Links
-  </div>
-    <div className="footer">
-      Footer
+  
     </div>
-    </div>
+    
     
   );
 }
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,9 +1,19 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
-const PhotoCard = () =>  {
-   
-   
 
-};
-export default PhotoCard;
+
+ function Photo(props){
+    return(
+        <>
+        {(props.info.media_type === "video") ?  
+        <iframe width="400" height="300" src={props.info.src}></iframe>
+        :
+        <img src={props.info.url} width="400" height="300"></img>}
+
+             <p className="date">{props.info.date}</p>
+            <p className="explanation">{props.info.explanation}</p>
+        </>
+    )
+}
+
+ export default Photo; 
